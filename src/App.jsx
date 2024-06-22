ab2b18d4d58ade9a127b4981ee3afd9da12f5fc7
kamalesh0211 <kamaleshmech77@gmail.com>
2024-06-22 18:26:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className=".Container">
+      <div className="MainNav">
+        <ul className="nav-list">
+          <li className="Home">Home</li>
+          <li className="Project">Project</li>
+          <li className="Education">Education</li>
+          <li className="Experience">Experience</li>
+          <li className="ContactUs">Contact Us</li>
+        </ul>
+      </div>
+      {/* <div className="summary"><p>Hi,m Kamaleshwaran</p></div> */}
+    </div>
+  );
+};
+
+export default App;
