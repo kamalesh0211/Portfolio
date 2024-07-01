8ef7004c9dd9a5dec4924957a6e403b9acc8a198
kamalesh0211 <kamaleshvivekanadan@gmail.com>
2024-07-02 00:28:24 +0530

    feat: edu edited

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -5,22 +5,27 @@ const Education = () => {
       <div className="section">
         <h5 className="title">SSLC</h5>
         <div className="paragraph">
-          <strong className="insitute">Annai Velankanni Matric.Sch</strong>
+          <strong className="insitute">
+            Annai Velankanni Matric.Sch Chennai
+          </strong>
           <br />
-          <p>07/2017 - 04/2019</p>
+          <p>07/2017</p>
         </div>
       </div>
       <div className="section">
         <h5 className="title">HSSC</h5>
         <div className="paragraph">
-          <strong className="insitute">Annai Velankanni Matric.Sch</strong>
+          <strong className="insitute">Velankanni Matric.Sch Chennai</strong>
           <br />
           <p>07/2017 - 04/2019</p>
         </div>
       </div>
       <div className="section">
         <h5 className="title">B.E (Electronic and Communication)</h5>
-        <p className="paragraph"></p>
+        <div className="paragraph">
+          <strong>University College of Engineering</strong> <br />
+          <p>06/2019 - 05/2023</p>
+        </div>
       </div>
     </section>
   );
