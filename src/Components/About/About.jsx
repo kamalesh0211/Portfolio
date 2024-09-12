caf1a395b97aa8b055269d234b7bc80e2a1b000a
kamalesh0211 <kamaleshvivekanadan@gmail.com>
2024-09-12 23:48:54 +0530

    feat: education faded(edited)

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,9 @@
 import "./About.css";
 import Skill from "../Skill/Skill";
 import Education from "../Education/Education";
+
+// option 2
+
 const About = () => {
   return (
     <section id="About">
@@ -9,6 +12,13 @@ const About = () => {
       <Skill />
     </section>
   );
+  window.addEventListener("scroll", function () {
+    const section = document.querySelector(".fade-section");
+    let scrollPosition = window.scrollY;
+    let opacityValue = 1 - scrollPosition / 500; // 500 can be adjusted
+    section.style.opacity = opacityValue > 0 ? opacityValue : 0;
+  });
+  
 };
 
 export default About;
