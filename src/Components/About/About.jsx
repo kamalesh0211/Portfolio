48054fd93eb912b42e8dc4a13ff56973fd330d73
kamalesh0211 <kamaleshvivekanadan@gmail.com>
2024-09-13 18:14:10 +0530

    feat(component) :modified

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,17 +7,17 @@ import Education from "../Education/Education";
 const About = () => {
   return (
     <section id="About">
-      <h2>Education & Skill</h2>
+      <h2>SKILL</h2>
       <Education />
       <Skill />
     </section>
   );
-  window.addEventListener("scroll", function () {
-    const section = document.querySelector(".fade-section");
-    let scrollPosition = window.scrollY;
-    let opacityValue = 1 - scrollPosition / 500; // 500 can be adjusted
-    section.style.opacity = opacityValue > 0 ? opacityValue : 0;
-  });
+  // window.addEventListener("scroll", function () {
+  //   const section = document.querySelector(".fade-section");
+  //   let scrollPosition = window.scrollY;
+  //   let opacityValue = 1 - scrollPosition / 500; // 500 can be adjusted
+  //   section.style.opacity = opacityValue > 0 ? opacityValue : 0;
+  // });
   
 };
 
--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -5,7 +5,7 @@ const Education = () => {
       <div className="section">
         <h1>About Me</h1>
         <p>
-          Hi there, I{"'"}m Vinoth a talented and passionate front-end developer
+          Hi there, I{"'"}m Kamaleshwaran a talented and passionate front-end developer
           with a keen eye for design and a deep understanding of user
           experience. With a strong background in HTML, CSS, and JavaScript and
           has a proven track record of creating engaging, responsive, and
