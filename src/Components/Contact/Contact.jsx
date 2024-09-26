d76a01a835693ef3c679c08a718f504e0cb4d1b3
kamalesh0211 <kamaleshvivekanadan@gmail.com>
2024-09-26 16:48:32 +0530

    feat: none

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,6 +1,12 @@
 import "./Contact.css";
 const Contact = () => {
-  return <section id='Contact'>Contact</section>;
+  return (
+    <section id="Contact">
+      <h2 className="contact">
+        <span>Contact </span>
+      </h2>
+    </section>
+  );
 };
 
 export default Contact;
